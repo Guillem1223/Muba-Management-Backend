2ed6ae76a4f4180bc164e30083cf9938aa4605a1
Guillem <saguesguillem@gmail.com>
2023-02-26 19:56:21 +0100

    feat: isSuperAdmin

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,14 @@ const performersController = require("../controllers/PerformersController.js");
 
 const usersController = require("../controllers/UsersController.js");
 const verifyToken = require("../middelwares/verifyToken.js");
+const isSuperAdmin = require("../middelwares/isSuperAdmin.js");
 var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
-router.get("/find/:role", verifyToken, usersController.findAll);
+router.get("/find/:role", usersController.findAll);
 
 // post users register
 router.post("/reg", AuthController.register);
@@ -24,9 +25,9 @@ router.post("/login", AuthController.login);
 // delete users
 router.delete("/delete/:id", usersController.deleteById);
 // update users
-router.put("/update/:id", AuthController.update);
+router.put("/update/:id", verifyToken, AuthController.update);
 
 //find by id
-router.get("/:id", performersController.getById);
+router.get("/:id", verifyToken, performersController.getById);
 router.get("/contractor/:id", contractorController.getById);
 module.exports = router;
